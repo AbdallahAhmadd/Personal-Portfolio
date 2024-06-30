fcbe7028131b4885b1f9a9f960faaa4e423c883f
Abdallah Ahmed <abdallah.ahmed@student.guc.edu.eg>
2024-06-30 19:16:22 +0300

    feat(UI): add icons and routes on click

--- src/Components/ProfileCard.jsx
+++ src/Components/ProfileCard.jsx
@@ -1,6 +1,17 @@
 import './profilecard.css'
 
 import Personal from '../assets/MyPic.jpeg'
+
+function handleGitClick(){
+    window.open('https://github.com/AbdallahAhmadd')
+}
+function handleLinkedInClick(){
+    window.open('https://www.linkedin.com/in/abdallah-ahmed-hassan/')
+}
+function handleInstagramClick(){
+    window.open('https://www.instagram.com/abdallahahmed___/')
+}
+
 function ProfileCard(){
     return(
       <div id={'container'}>
@@ -23,10 +34,10 @@ function ProfileCard(){
           </div>
 
           <div id={'content'}>
-                <button>Projects</button>
-                <button>Github</button>
-                <button>LinkedIn</button>
-                <button>Instagram</button>
+              <button>Projects</button>
+              <button onClick={handleGitClick}>Github<i className="fa-brands fa-github"></i> </button>
+              <button onClick={handleLinkedInClick}>LinkedIn<i className="fa-brands fa-linkedin"></i></button>
+              <button onClick={handleInstagramClick}>Instagram<i className="fa-brands fa-instagram"></i></button>
           </div>
 
       </div>
