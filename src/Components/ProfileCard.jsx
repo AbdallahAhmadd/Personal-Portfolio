8d2a5b8fc244189b1a50f5c2e99007998bdd5881
Abdallah Ahmed <abdallah.ahmed@student.guc.edu.eg>
2024-06-30 20:36:00 +0300

    feat(UI): expand container and remove divs

--- src/Components/ProfileCard.jsx
+++ src/Components/ProfileCard.jsx
@@ -1,6 +1,7 @@
 import './profilecard.css'
 
 import Personal from '../assets/MyPic.jpeg'
+import {useState} from "react";
 
 function handleGitClick(){
     window.open('https://github.com/AbdallahAhmadd')
@@ -12,11 +13,20 @@ function handleInstagramClick(){
     window.open('https://www.instagram.com/abdallahahmed___/')
 }
 
+
 function ProfileCard(){
+    const [isProject,setIsProject] = useState(true)
+
+    function toggleExpand() {
+        const container = document.getElementById('container');
+        container.classList.toggle('expanded');
+        setIsProject(false);
+    }
+
     return(
       <div id={'container'}>
 
-          <div id={'header'}>
+          {isProject && (<div id={'header'}>
 
             <div id={'logo'}>
                 <img src={Personal} alt={'Photo of the author'}/>
@@ -31,14 +41,14 @@ function ProfileCard(){
                 <h4>Computer Engineering Student at the<br/>German University in Cairo</h4>
             </div>
 
-          </div>
+          </div>)}
 
-          <div id={'content'}>
-              <button>Projects</button>
+          {isProject && (<div id={'content'}>
+              <button onClick={toggleExpand}>Projects</button>
               <button onClick={handleGitClick}>Github<i className="fa-brands fa-github"></i> </button>
               <button onClick={handleLinkedInClick}>LinkedIn<i className="fa-brands fa-linkedin"></i></button>
               <button onClick={handleInstagramClick}>Instagram<i className="fa-brands fa-instagram"></i></button>
-          </div>
+          </div>)}
 
       </div>
     );
