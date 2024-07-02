5d8eb3ff736978e3a9c9c18a84ce43b14f306c2c
Abdallah Ahmed <abdallah.ahmed@student.guc.edu.eg>
2024-07-02 11:57:06 +0300

    feat(ui): add landing page component in main

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import ProfileCard from "./Components/ProfileCard.jsx";
 import LandingPage from "./Pages/LandingPage.jsx";
-import Skills from "./Components/Skills/Skills.jsx";
 
 function App() {
 
   return (
     <>
-      <Skills/>
+      <LandingPage />
     </>
   )
 }
